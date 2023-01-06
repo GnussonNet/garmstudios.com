69d28b9f1cc0e094584493ae57ba9df6bd517770
GnussonNet <admin@gnusson.net>
2023-01-06 01:33:32 +0100

    Added color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "garm-blue": "#1d3348",
+        "garm-light-blue": "#9bb5c4",
+        "garm-gold": "#d5c296",
+      },
+    },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /(bg|text|border)-garm-(blue|light-blue|gold)/,
+    },
+  ],
 };
