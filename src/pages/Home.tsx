75bd74a182fa504832926679d8ecf24c6363c46e
GnussonNet <admin@gnusson.net>
2023-03-06 21:22:10 +0100

    Added views to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+// import background from "../assets/background.png"
+import CallToActionView from "../views/CallToActionView";
+import ContactView from "../views/ContactView";
+import TrustView from "../views/TrustView";
 
 function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl">Garm Studios</h1>
-    </div>
-  );
+  return <div className="">
+    {/* <div className="relative">
+      <img className="bg-cover h-screen" src={background} alt="background" />
+      <div className="container mx-auto px-4 absolute bottom-20 left-16">
+        <h1 className="text-8xl font-extrabold">We<br /><span className="text-[#B8D4E1]">Capture</span><br />Moments.</h1>
+      </div>
+    </div> */}
+
+    <div className="h-screen"></div>
+
+    <TrustView />
+
+    <ContactView />
+
+    <CallToActionView />
+
+  </div>;
 }
 
 export default Home;
