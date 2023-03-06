db87b043db5c1b608fe43758c28263be219427b9
GnussonNet <admin@gnusson.net>
2023-03-06 21:17:10 +0100

    Added custom media queries, garm theme and forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    container: {
+      center: true, 
+    },
     extend: {
       colors: {
-        "garm-blue": "#1d3348",
-        "garm-light-blue": "#9bb5c4",
-        "garm-gold": "#d5c296",
+        "garm-blue-900": "#0F2C3E",
+        "garm-blue-800": "#112E40",
+        "garm-blue-600": "#1F4C63",
+        "garm-blue-500": "#1E5772",
+        "garm-blue-200": "#82A6B6",
+        "garm-blue-100": "#A3C3D0",
+        "garm-gold-700": "#E2B87C",
+        "garm-gold-500": "#DCCB9D",
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
   safelist: [
     {
       pattern: /(bg|text|border)-garm-(blue|light-blue|gold)/,
