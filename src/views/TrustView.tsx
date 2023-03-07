5f8520267395372c5ba5ca4f9597d66823e81a5f
GnussonNet <admin@gnusson.net>
2023-03-07 13:01:35 +0100

    Added real client logos, still need color change

--- src/views/TrustView.tsx
+++ src/views/TrustView.tsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import codic from '../assets/codic.png'
+import drikk from '../assets/drikk.png'
+import elon from '../assets/elon.png'
+import fs from '../assets/fs.png'
+import gomo from '../assets/gomo.png'
+import obstecare from '../assets/obstecare.png'
+import sws from '../assets/sws.png'
+import vv from '../assets/vv.png'
 
 const TrustView = () => {
   return (
@@ -9,24 +17,30 @@ const TrustView = () => {
             Trusted by well-known companies
           </h1>
         </div>
-        <div className="grid grid-cols-2 gap-0.5 md:grid-cols-6 pt-6">
+        <div className="grid grid-cols-2 gap-0.5 md:grid-cols-8 pt-6">
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/2a4d2cdd794587314ad2034778712608ac32e37c/79f3b/images/logos/8.svg" alt="logo" />
+            <img className="max-h-16" src={codic} alt="logo" />
           </div>
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/aae3d6dfaee9138c485f5305dd33b7f80379edb4/64dd2/images/logos/2.svg" alt="logo" />
+            <img className="max-h-16" src={drikk} alt="logo" />
           </div>
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/4dc5df63255f9f0c1f54c804dd3149cf11308507/b7a70/images/logos/3.svg" alt="logo" />
+            <img className="max-h-16" src={elon} alt="logo" />
           </div>
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/be7130b04bb6b932ed9222877a5e9146d80c0eba/6511d/images/logos/4.svg" alt="logo" />
+            <img className="max-h-16" src={fs} alt="logo" />
           </div>
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/456c999508e76cd199714cfa4fad3826ebb02216/9147b/images/logos/5.svg" alt="logo" />
+            <img className="max-h-16" src={gomo} alt="logo" />
           </div>
           <div className="flex justify-center col-span-1 px-8">
-            <img className="max-h-12" src="https://d33wubrfki0l68.cloudfront.net/b5d09ea7476a226d10dd1235e071288761e51da7/e68ac/images/logos/6.svg" alt="logo" />
+            <img className="max-h-16" src={obstecare} alt="logo" />
+          </div>
+          <div className="flex justify-center col-span-1 px-8">
+            <img className="max-h-16" src={sws} alt="logo" />
+          </div>
+          <div className="flex justify-center col-span-1 px-8">
+            <img className="max-h-16" src={vv} alt="logo" />
           </div>
         </div>
 
